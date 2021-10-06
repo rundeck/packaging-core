b0d2f94f7252338f80921e6733a38f00f29172ae
Chris McCarroll-Gilbert <chrismcg@rundeck.com>
2021-10-06 11:58:27 -0600

    update shim

--- .circleci/travis-shim.sh
+++ .circleci/travis-shim.sh
@@ -1,7 +1,8 @@
+#!/bin/sh
 export TRAVIS_BRANCH=${CIRCLE_UPSTREAM_BRANCH:-}
 export UPSTREAM_TAG=${CIRCLE_UPSTREAM_TAG:-}
 export TRAVIS_BUILD_NUMBER=${CIRCLE_PIPELINE_NUM:-}
 export TRAVIS_COMMIT=${CIRCLE_SHA1:-}
 
-S3_ARTIFACT_BASE="s3://rundeck-ci-artifacts/oss/circle/rundeck"
-ECR_IMAGE_PREFIX="circle"
+#S3_ARTIFACT_BASE="s3://rundeck-ci-artifacts/oss/circle/rundeck"
+#ECR_IMAGE_PREFIX="circle"
